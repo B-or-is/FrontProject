365293ed703ee0119d617fd6783d95fa03b90893
B_or_is <bbb.freemail@gmail.com>
2022-05-24 17:49:35 +0300

    Added Jinia Template and Django Template tests to the main menu.

--- jinja_app/urls.py
+++ jinja_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from jinja_app import views
 
 urlpatterns = [
-    path('jinja2/', views.jinja_test),
-    path('django/', views.django_test),
+    path('jinja2/', views.jinja_test, name='jinja2'),
+    path('django/', views.django_test, name='django'),
 ]
 
 
